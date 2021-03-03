5a260732d6442ce3ed0496fa06601855c160b7e8
yuchengH1988 <yucheng.h.1988@gmail.com>
2021-03-03 11:07:40 +0800

    feat : npm i express, git init

---
+++ app.js
@@ -0,0 +1,26 @@
+// app.js
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.send('列出全部 Todo')
+})
+
+app.get('/new', (req, res) => {
+  res.send('新增 Todo 頁面')
+})
+
+app.get('/:id', (req, res) => {
+  res.send('顯示一筆 Todo')
+})
+
+app.post('/', (req, res) => {
+  res.send('新增一筆  Todo')
+})
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}`)
+})
